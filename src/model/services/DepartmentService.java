2c05ae3d462649964d616b5504589efc90ebb38b
Alex-Trauthman <alextrauthman.full@gmail.com>
2024-05-19 21:02:14 -0300

    Data acces included based in jdbc-DAO (another project)

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -1,16 +1,16 @@
 package model.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
+	
+	private DepartmentDao depDao = DaoFactory.createDepartmentDao();
+	
 	public List<Department> findAll(){
-		List<Department> departmentList = new ArrayList<>();
-		departmentList.add(new Department(1,"Eletronics"));
-		departmentList.add(new Department(2,"Books"));
-		departmentList.add(new Department(3, "Celphones"));
-		return departmentList;
+		return depDao.findAll();
 	}
 }
